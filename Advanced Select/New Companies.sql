1eb8543d8994ebfae6d3144607c5640fb50a4219
Vishali S <100502539+VishaliSenthilkumar@users.noreply.github.com>
2023-06-19 19:43:37 +0530

    Update New Companies.sql

--- Advanced Select/New Companies.sql
+++ Advanced Select/New Companies.sql
@@ -12,3 +12,16 @@ sm.senior_manager_code=m.senior_manager_code AND
 m.manager_code=e.manager_code
 GROUP BY c.company_code,c.founder
 ORDER BY c.company_code ASC
+
+#Another way
+SELECT company.company_code, 
+       founder, 
+       Count(DISTINCT lead_manager_code), 
+       Count(DISTINCT senior_manager_code), 
+       Count(DISTINCT manager_code), 
+       Count(DISTINCT employee_code) 
+FROM company 
+       JOIN (employee) 
+         ON ( company.company_code = employee.company_code ) 
+GROUP BY company_code,founder
+ORDER BY company_code
